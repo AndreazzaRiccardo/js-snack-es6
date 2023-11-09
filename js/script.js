5e28e523b8662576d8fd6ad88995fe80f2c51e3c
AndreazzaRiccardo <riccardo.andreazza90@gmail.com>
2023-11-09 10:46:36 +0100

    fix: exercise correction

--- js/script.js
+++ js/script.js
@@ -20,21 +20,34 @@ const bikes = [
     }
 ];
 
-let lightWeight = bikes[0].peso;
-let lightBike   = bikes[0].nome;
-
-for (let i = 0; i < bikes.length; i++) {
-
-    if(bikes[i].peso <= lightWeight){
-        lightBike   = bikes[i].nome;
-        lightWeight = bikes[i].peso;
+let lightBike = bikes[0];
+bikes.forEach((curBike) => {
+    if(curBike.peso < lightBike.peso){
+        lightBike = curBike;
     }
-}
+})
 
-document.querySelector(".container-1").innerHTML +=`
-    <p>Nome: ${lightBike}</p>
-    <p>Peso: ${lightWeight}</p>
+const {nome, peso} = lightBike;
+document.querySelector(".container-1").innerHTML = `
+    La bici piu leggera è ${nome} con peso di ${peso}
 `;
+
+
+// let lightWeight = bikes[0].peso;
+// let lightBike   = bikes[0].nome;
+
+// for (let i = 0; i < bikes.length; i++) {
+
+//     if(bikes[i].peso <= lightWeight){
+//         lightBike   = bikes[i].nome;
+//         lightWeight = bikes[i].peso;
+//     }
+// }
+
+// document.querySelector(".container-1").innerHTML +=`
+//     <p>Nome: ${lightBike}</p>
+//     <p>Peso: ${lightWeight}</p>
+// `;
 
 
 // ***********************************************************************************
@@ -69,26 +82,43 @@ const teams = [
     }
 ];
 
-let newArrayTeams = [];
-
-teams.forEach(team => {
-    team.puntiFatti  = generateRndNumber();
-    team.falliSubiti = generateRndNumber();
-
-    let {nome, puntiFatti, falliSubiti} = team;
-    newArrayTeams.push({
-        nome,
-        falliSubiti
-    });
-
-    console.log(team);
-});
-
-console.log(newArrayTeams);
-
 function generateRndNumber(){
     return Math.floor(Math.random() * 50) + 1;
 }
+
+teams.forEach((team) => {
+    team.falliSubiti = generateRndNumber();
+    team.puntiFatti = generateRndNumber();
+})
+
+console.log(teams);
+
+const newTeams = teams.map((team) => {
+    const {nome, falliSubiti} = team;
+    return {
+            nome,
+            falliSubiti
+           };
+})
+
+console.log(newTeams);
+
+// let newArrayTeams = [];
+
+// teams.forEach(team => {
+//     team.puntiFatti  = generateRndNumber();
+//     team.falliSubiti = generateRndNumber();
+
+//     const {nome, puntiFatti, falliSubiti} = team;
+//     newArrayTeams.push({
+//         nome,
+//         falliSubiti
+//     });
+
+//     console.log(team);
+// });
+
+// console.log(newArrayTeams);
 
 
 // ***********************************************************************************
@@ -104,16 +134,31 @@ const myArray = ["Cane", "Gatto", "Coniglio", "Criceto", "Serpente"];
  * @param {number} endNumber
  * @returns {array} newArray
  */ function getSubArray (array, startNumber, endNumber) {
-    let newArray = [];
-    array.forEach((currElem, index) => {
-        if(index >= startNumber && index <= endNumber){
-            newArray.push(currElem);
-        } 
-    });
-    return newArray;
+        const newArray = [];
+        array.forEach((currElem, index) => {
+            if(index >= startNumber && index <= endNumber){
+                newArray.push(currElem);
+            } 
+        });
+        return newArray;
+    }
+
+ console.log(getSubArray(myArray, 1, 3));
+
+
+
+ function getSubArray2 (array, startNumber, endNumber) {
+    // const newArray = array.filter((currElem, index) => {
+    //     return (index >= startNumber && index <= endNumber)
+    // });
+    // return newArray
+    if(endNumber > array.length || startNumber > array.length || startNumber > endNumber) {
+        return ["Error"];
+    }
+    return array.filter((currElem, index) => index >= startNumber && index <= endNumber);
  }
 
- console.log(getSubArray(myArray, 4, 1));
+ console.log(getSubArray2(myArray, 2, 4));
 
 
 
